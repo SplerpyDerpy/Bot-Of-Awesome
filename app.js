badf467f022a5d38a5dd6aa03fb8ed465c3a444b
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-04-20 22:38:20 +1000

    Update app.js

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ client.on('message', message => {
            
 });
 
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-
-
 client.on('message', message => {
     if (message.content === 'Spam') {
         message.author.sendMessage("please do not spam");
