146674f79e4f82fbc71c1000611975a5797144a5
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-04-20 23:27:16 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
- const args = message.content.slice(prefix.length).trim().split(/ +/g);
+ const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
 
 if(command === 'ping') {
