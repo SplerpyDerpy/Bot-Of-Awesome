e2b6722693d119c24dca9a8aeb91e6d718d4aca7
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-04-21 08:55:06 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+const prefix = "+",
+
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
- const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+ const args = message.content.slice(prefix.length).trim().split(/ +/g);
  const command = args.shift().toLowerCase();
 
 client.on('message', message => {
