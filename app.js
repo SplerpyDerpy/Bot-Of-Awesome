4e2aca42a619d7e5bf63874b327565ce56ba23e5
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-04-20 20:51:19 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,82 +1,20 @@
-
-
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-const config = require("./config.json");
-
-        
-      
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-
-
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-  	}
-});
-
-
-client.on('message', message => {
-    if (message.content === 'Bidoof') {
-        message.channel.send('oof')
-    }
-});
-
-client.on('message', message => {
-    if (message.content === 'Spam') {
-        message.author.sendMessage('do not spam and if you typed spam for some other reason then you are an intellect')
-    }
-});
-
-
-client.on('message', message => {
-
+    }else
+            
     if (message.content === 'Oof') {
-
-        message.channel.send('bidoof')
-
+        message.reply('bidoof');
     }
-
+           
 });
-
-
-
-client.on("message", async message => {
-
-    if(message.author.bot) return;
-
-    if(message.content.indexOf(config.prefix) !== 0) return;
-
-
-
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-
-const command = args.shift().toLowerCase();
-
-    
-
-if(command === "say") {
-
-    const sayMessage = args.join(" ");
-
-    message.delete().catch(hmm=>{});
-
-    message.channel.send(sayMessage);
-
-}
-
-});
-
-
 
 // THIS  MUST  BE  THIS  WAY
-
-
-
 client.login(process.env.BOT_TOKEN);
