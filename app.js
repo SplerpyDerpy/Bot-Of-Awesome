454540baaa8ea6386225940aaf5336637fbf2a46
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-04-20 17:19:33 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,52 +1,39 @@
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
-
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
 });
-
-
-if (message.substring(0, 1) == '!') {
-
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
-
         var cmd = args[0];
-    
-    
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-    	message.reply('pong');
-
-  	}
-
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
 });
-
-client.on('message', message => {
-    if (message.content === 'oof') {
-        message.reply('bidoof');
-    }
-})
-
-
-args = args.splice(1);
-    switch(cmd)
-        case 'ping':
-        bot.sendMessage({
-                to: channelID,
-                    message: "pong"
-        });
-    break;
-    case 'spam':
-    message.author.sendMessage('bamboozled, spamming is a horrible thing to do why would you even consider it')
-    
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);
