cccf63f4aee06a71c5d2acdfc021330e04476860
SplerpyDerpy <38550953+SplerpyDerpy@users.noreply.github.com>
2018-04-21 10:34:08 +1000

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,19 @@ client.on('message', message => {
     }
     if (command === 'say') {
         const sayMessage = args.join(" ");
-        message.delete().catch(O_o=>{});
+        message.delete();
         message.channel.sendMessage(sayMessage);
     }
     if (command === 'roast') {
-        message.channel.sendMessage("ummm" + message.mentions.members.first() + "is gay lol");
+        message.channel.sendMessage("ummm " + message.mentions.members.first() + " is gay lol");
+    }
+    if (command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     }
         
 });
